2c09a963adefc2ff02b1ae6d96cc9f5705d56e41
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-09 00:57:56 +0530

    Add project files.

---
+++ Merge to arrays.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_the_digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 1, 2, 3, 4, 5 };
+            int[] ar1 = { 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write( + ar[i]);
+                Console.Write(",");
+            }
+
+            for (int j = 0; j < ar1.Length; j++)
+            {
+                Console.Write(ar1[j]);
+                Console.Write(",");
+            }
+                          
+
+                        }
+
+                    }
+                }
+            
+        
+    
+
+    
+
+
+    
+
+
